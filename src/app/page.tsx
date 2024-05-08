61883ad0bd617ac1a1df6116f1e3283d77419734
arndom <aminonimisi2@gmail.com>
2024-05-08 10:07:42 +0100

    figure out ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import { images } from "@/data";
+import Image from "next/image";
+
 export default function Home() {
-  const sectionClassName = "flex h-screen flex-col items-center justify-center text-center";
+  const sectionClassName = "flex min-h-screen flex-col items-center justify-center text-center";
 
   return (
     <main className="min-h-screen">
       <div className="navbar fixed top-0 w-full p-8 z-10 flex justify-between font-bold uppercase text-sm">
         <a href="#">Gallery Demo</a>
-        <a href="#">Credits</a>
+        <a href="#credits">Credits</a>
       </div>
 
       <section className={`${sectionClassName} bg-white`}>
-        <div>
+        <div className="banner">
           <p className="font-thin text-xs md:text-sm lg:text-md -mb-4 md:-mb-8 lg:-mb-12 font-serif italic text-right w-full">
             Netlify
           </p>
@@ -20,16 +25,41 @@ export default function Home() {
 
           <p className="text-md mt-3 md:mt-4 lg:mt-6">
             curated by{" "}
-            <a href="#" className="underline underline-offset-4">@arndom</a>
+            <a
+              href="https://github.com/arndom"
+              target="__blank"
+              className="underline underline-offset-4"
+            >
+              @arndom
+            </a>
           </p>
         </div>
       </section>
 
-      <section className={`${sectionClassName} bg-black`}></section>
+      <section id="slides" className={`${sectionClassName} bg-black py-8 overflow-hidden`}>
+        <div className="slides flex gap-4 px-4 py-24 items-center overflow-x-auto">
+          {images.map((img, ind) => (
+            <Image
+              src={img.image}
+              key={img.image}
+              alt={"slide-" + (ind + 1)}
+              width={280}
+              height={280}
+              objectFit="cover"
+              quality={100}
+            />
+          ))}
+        </div>
+      </section>
 
-      <button className="chapters-btn fixed bottom-10 left-1/2 -translate-x-1/2 z-10 px-[1.25rem] py-[.6875rem] rounded-full font-extrabold uppercase text-sm">
-        <p>Chapters</p>
-      </button>
+      <section
+        id="credits"
+        className={`${sectionClassName} bg-white`}
+      ></section>
+
+      <a href="#slides" className="chapters-btn fixed bottom-10 left-1/2 -translate-x-1/2 z-10 px-[1.25rem] py-[.6875rem] rounded-full font-extrabold uppercase text-sm">
+        Content
+      </a>
     </main>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,78 @@
+export const images = [
+  {
+    image: "/images/1.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@_miltiadis_?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Miltiadis Fragkidis</a> on <a href="https://unsplash.com/photos/brown-trees-on-sidewalk-during-daytime-U88iyzoZdlY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `,
+  },
+  {
+    image: "/images/2.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@jonasjacobsson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Jonas Jacobsson</a> on <a href="https://unsplash.com/photos/road-near-houses-d2dbqAIX6Ws?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/3.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@milica_spasojevic?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Milica Spasojevic</a> on <a href="https://unsplash.com/photos/brown-concrete-building-beside-road-lt6SR2P13wc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/4.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@unstable_affliction?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Ivan Bandura</a> on <a href="https://unsplash.com/photos/aerial-view-photography-of-vehicles-and-buildings-during-daytime-0-no6ywKMPY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/5.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@anthonyreungere?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Anthony Reungère</a> on <a href="https://unsplash.com/photos/buildings-on-side-streets-s6xt1mwF_iU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/6.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@mihail_ribkin?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Mihail Ribkin</a> on <a href="https://unsplash.com/photos/aerial-view-of-buildings-26yvIaheDMU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+
+  {
+    image: "/images/7.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@draufsicht?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Andreas Gücklhorn</a> on <a href="https://unsplash.com/photos/birds-eyeview-of-city-7nV4CZSIoxg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/8.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@mikeliaophotography?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Mike L</a> on <a href="https://unsplash.com/photos/blue-fishing-vessel-near-houses-on-mountain-GQe23-3-Lf8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/9.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@brunus?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Bruno Martins</a> on <a href="https://unsplash.com/photos/white-and-brown-concrete-building-beside-brown-leaf-tree-under-clear-blue-sky-GkZvxVsHYWw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/10.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@colinextremis?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Coline Beulin</a> on <a href="https://unsplash.com/photos/worms-eye-view-of-tower-oLWGI-Q76Yc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/11.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@victor_g?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Victor</a> on <a href="https://unsplash.com/photos/low-angle-photography-of-building-6SVXB68MhI0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/12.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@samule?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Samule Sun</a> on <a href="https://unsplash.com/photos/village-artwork-lqISplTobR8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/13.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@rarchitecture_melbourne?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">R ARCHITECTURE</a> on <a href="https://unsplash.com/photos/brown-and-black-concrete-building-JvQ0Q5IkeMM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/14.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@rarchitecture_melbourne?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">R ARCHITECTURE</a> on <a href="https://unsplash.com/photos/brown-and-white-wooden-house-near-green-trees-during-daytime-2gDwlIim3Uw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+  {
+    image: "/images/15.jpg",
+    attr: `Photo by <a href="https://unsplash.com/@phito?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Philipp Torres</a> on <a href="https://unsplash.com/photos/people-walking-on-sidewalk-between-buildings-during-daytime-bGaj8lcmbL8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="__blank">Unsplash</a>
+    `
+  },
+]
